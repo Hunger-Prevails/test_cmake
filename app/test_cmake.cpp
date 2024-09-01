0bb96ceb486675342280acd31f4e7eab851792cf
Hunger-Prevails <yinglun.liu.it.happens@gmail.com>
2024-09-01 22:07:47 +0200

    epic build & run on manifest mode

--- app/test_cmake.cpp
+++ app/test_cmake.cpp
@@ -1,12 +1,32 @@
 # include <iostream>
 # include <fstream>
 
+# include <cxxopts.hpp>
+# include <fmt/format.h>
+# include <range/v3/view.hpp>
+
 # include "person.h"
 # include "json.h"
 
+namespace view = ranges::views;
 using namespace std;
 
-int main() {
+int fib(int x)
+{
+	int a = 0, b = 1;
+
+	for (int it : view::repeat(0) | view::take(x))
+	{
+		(void)it;
+		int tmp = a;
+		a += b;
+		b = tmp;
+	}
+
+	return a;
+}
+
+int main(int argc, char **argv) {
 	Person person_a("Alice");
 	Person person_b("Bob");
 
@@ -22,5 +42,15 @@ int main() {
 	fout << root;
 	fout.close();
 
+	cxxopts::Options options("fibo", "Print the fibonacci sequence up to a value 'n'");
+	options.add_options()("n,value", "The value to print to", cxxopts::value<int>()->default_value("10"));
+
+	auto result = options.parse(argc, argv);
+	auto n = result["value"].as<int>();
+
+	for (int x: view::iota(1) | view::take(n))
+	{
+		fmt::print("fib({}) = {}\n", x, fib(x));
+	}
 	return 0;
 }
